4f6ca7c80726138aa7531172e6196f44baa15729
JohhniV <59938017+JohhniV@users.noreply.github.com>
2022-04-02 13:53:47 +0300

    Update defineJS.js

--- js/defineJS.js
+++ js/defineJS.js
@@ -4,11 +4,7 @@
     src.setAttribute("src", jsfile);
     document.getElementsByTagName("body")[0].appendChild(src);
   };
-
-// Wait until document ready
-$("#navbarTop").ready(function() {
  // Add script on to the page
  addJS("/js/script.js");
  // Add fslightbox on to the page
  addJS("/js/fslightbox.js");
-});
